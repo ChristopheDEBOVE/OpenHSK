4cb532d06238a66173940dce1c547dbbe4bcfffa
ChristopheDEBOVE <34914918+ChristopheDEBOVE@users.noreply.github.com>
2021-05-03 21:08:01 +0200

    Added the example class (#8)

---
+++ OpenHSK.Domain.Test/Features/Example.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="Example.cs" company="openhsk.com">
+//     Open Hsk
+// </copyright>
+// <author>Christophe DEBOVE</author>
+
+namespace OpenHSK.Domain
+{
+    /// <summary>
+    /// In order to practice the exam good examples are mandatories
+    /// </summary>
+    public class Example
+    {
+        public Example(NotEmptyText text, HskLevel level, Student student)
+        {
+            this.Text = text;
+            this.Level = level;
+            this.Author = student;
+        }
+
+        public Student Author { get; }
+
+        public HskLevel Level { get; }
+
+        public NotEmptyText Text { get; }
+    }
+}
---
+++ OpenHSK.Domain.Test/Features/NotEmptyText.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="NotEmptyText.cs" company="openhsk.com">
+//     Open Hsk
+// </copyright>
+// <author>Christophe DEBOVE</author>
+
+namespace OpenHSK.Domain
+{
+    using System;
+    using System.Collections.Generic;
+    using CSharpFunctionalExtensions;
+
+    public class NotEmptyText : ValueObject
+    {
+        public string Value { get; }
+
+        public NotEmptyText(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("An NotEmptyText must be construct with a not empty value", nameof(value));
+            }
+            Value = value;
+        }
+
+        public static implicit operator NotEmptyText(string v) => new NotEmptyText(v);
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Value;
+        }
+    }
+}
