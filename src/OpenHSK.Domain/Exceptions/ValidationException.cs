bb4ef41a20ea1df195b349ab00df1d6263e3909e
Christophe DEBOVE <cdebove15@gmail.com>
2021-05-04 23:36:45 +0200

    Fix Code smell on custom exception making it serializable

--- src/OpenHSK.Domain/Exceptions/ValidationException.cs
+++ src/OpenHSK.Domain/Exceptions/ValidationException.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Runtime.Serialization;
-
-namespace OpenHSK.Application.Examples.Commands
+﻿namespace OpenHSK.Application.Examples.Commands
 {
-    public class ValidationException : Exception, ISerializable
+    using System;
+    using System.Runtime.Serialization;
+    using System.Security.Permissions;
+
+    [Serializable]
+    public class ValidationException : Exception
     {
         public ValidationException(string message) : base(message)
         {
+        }
+
+        [SecurityPermissionAttribute(SecurityAction.Demand, SerializationFormatter = true)]
+        protected ValidationException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        [SecurityPermissionAttribute(SecurityAction.Demand, SerializationFormatter = true)]
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
         }
     }
 }
